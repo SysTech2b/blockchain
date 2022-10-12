10b923aa37b62eb047e823f746d62ced34e3c2e0
Spectre-Dragon <alan168@gmx.de>
2022-10-12 09:41:13 +0000

    zweiter Block

--- block.js
+++ block.js
@@ -21,6 +21,12 @@ class Block{
         Data:       ${this.data}`
     }
 
+    static genesis(){ // 1. Block der chain
+
+        return new this("Genesis time","-----","xABc1234",["data1","data2,data3"]); // call constructor
+
+    }
+
 }
 
 module.exports = Block; // Export als Modul
--- dev-test.js
+++ dev-test.js
@@ -6,4 +6,6 @@ const Block = require("./block");
 const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
 console.log(block.toString());
 
+console.log(Block.genesis().toString());
+
 // console.log("Hello, world!");
